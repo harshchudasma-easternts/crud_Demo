239105925be82678821e1c8ea36343f21992c851
harsheasternts <107181874+harsheasternts@users.noreply.github.com>
2022-06-15 09:05:40 +0530

    web commit by harsh chudasma

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:animation_demo/screens/form_screens/screens/AdmissionForm.dart';
 import 'package:animation_demo/screens/list_screen/screens/list_screen.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+    // enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+  );
 } 
 
 class MyApp extends StatelessWidget {
@@ -13,6 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      builder: DevicePreview.appBuilder,
       title: "CRUD Demo",
       debugShowCheckedModeBanner: false,
       routes: {
