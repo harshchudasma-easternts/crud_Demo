0c10fdb8b4405fd5b26d2fddd30c500c19ef6528
harsheasternts <107181874+harsheasternts@users.noreply.github.com>
2022-07-05 17:38:23 +0530

    tuesday commit by me

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
-import 'dart:io';
-
-import 'package:animation_demo/dbhelper/hive_database/hive_model.dart';
 import 'package:animation_demo/provider_demo.dart/database_provider.dart';
 import 'package:animation_demo/screens/form_screens/screens/AdmissionForm.dart';
 import 'package:animation_demo/screens/list_screen/screens/list_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as pathProvider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Directory directory = await pathProvider.getApplicationDocumentsDirectory();
-  Hive.init(directory.path);
-  Hive.registerAdapter(ProfessorsAdapter());
   runApp(
     MyApp(),
   );
